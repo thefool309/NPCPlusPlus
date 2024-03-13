b23b62852c8174899fd0aa7ea33d6d20e39e6580
thefool309 <themagician309@gmail.com>
2024-03-13 02:50:45 -0500

    Finished rogue class

--- NPCPlusPlus/Classes/Rogue/Rogue.cpp
+++ NPCPlusPlus/Classes/Rogue/Rogue.cpp
@@ -1,1 +1,25 @@
 #include "Rogue.h"
+
+void Rogue::DetermineSneakAttack()
+{
+	if (Lvl() > 0 && Lvl() < 3)
+		mNumOfSneakAttackDice = 1;
+	else if (Lvl() >= 3 && Lvl() < 5)
+		mNumOfSneakAttackDice = 2;
+	else if (Lvl() >= 5 && Lvl() < 7)
+		mNumOfSneakAttackDice = 3;
+	else if (Lvl() >= 7 && Lvl() < 9)
+		mNumOfSneakAttackDice = 4;
+	else if (Lvl() >= 9 && Lvl() < 11)
+		mNumOfSneakAttackDice = 5;
+	else if (Lvl() >= 11 && Lvl() < 13)
+		mNumOfSneakAttackDice = 6;
+	else if (Lvl() >= 13 && Lvl() < 15)
+		mNumOfSneakAttackDice = 7;
+	else if (Lvl() >= 15 && Lvl() < 17)
+		mNumOfSneakAttackDice = 8;
+	else if (Lvl() >= 17 && Lvl() < 19)
+		mNumOfSneakAttackDice = 9;
+	else
+		mNumOfSneakAttackDice = 10;
+}
--- NPCPlusPlus/Classes/Rogue/Rogue.h
+++ NPCPlusPlus/Classes/Rogue/Rogue.h
@@ -3,5 +3,15 @@
 class Rogue :
     public Character
 {
+private:
+	int mNumOfSneakAttackDice;
+public:
+	Rogue(int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma, int lvl, int hitDie, string name)
+		: Character(strength, dexterity, constitution, intelligence, wisdom, charisma, lvl, hitDie, name)
+	{
+		DetermineSneakAttack();
+	}
+
+	void DetermineSneakAttack();
 };
 
